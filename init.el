c4e9d5e2ba62ee9d5d28b8e0e7eb60e1c618b7b9
M0lion <bjorn-moholt@hotmail.com>
2021-12-02 17:17:12 +0100

    foo

--- init.el
+++ init.el
@@ -1,228 +1,47 @@
+;; Turn off beep
+(setq visible-bell t)
 
-;; Initialize package sources
-(require 'package)
+;; Loads module if file exists in config directory
+(defun try-load (module)
+  (let ((file (expand-file-name (concat (symbol-name module) "-module.el") user-emacs-directory)))
+    (when (file-exists-p file)
+      (load file))))
 
+;; Check for proxy settings
+(try-load 'proxy)
+
+;; Set package repos
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
+;; Setup use-package
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-
-(require 'use-package)
 (setq use-package-always-ensure t)
-;; End Package init
-
-(setq inhibit-startup-message t); Disable startup message
-
-(scroll-bar-mode -1); Disable visible scrollba
-(tool-bar-mode -1); Disable toolbar
-(tooltip-mode -1); Disable tooltips
-(set-fringe-mode 10); Breathing room?
-
-(menu-bar-mode -1); Disable menu bar
-
-(setq visible-bell t) ; Visible bell(not beep)?
-
-;; Make ESC quit propmpts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Set file for auto generated customise stuff
 (setq custom-file "~/.emacs.d/custom.el")
 (unless (file-exists-p custom-file)
   (write-region "" nil custom-file))
+
+;; Load Custom stuff
+(setq custom-file "~/.emacs.d/custom.el")
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
 (load custom-file)
 
-(defun font-exists-p (font)
-  (cl-labels ((font-exists-r (font-list)
-			     (when font-list
-				 (if (string= font (car font-list))
-				     t
-				   (font-exists-r (cdr font-list))))))
-    (font-exists-r (font-family-list))))
-
-(when (font-exists-p "Courier New")
-  (set-face-attribute 'default nil
-		      :font "Courier New"
-		      :weight 'semi-bold
-		      :height 110))
-
-(load-theme 'wombat)
-
-;; Enable line numbers
-(column-number-mode)
-(global-display-line-numbers-mode t)
-
-;; Disable line numbers for som modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(use-package doom-themes
-  :init (load-theme 'doom-gruvbox t))
-
-(use-package ivy
-  :bind (:map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :demand
-  :config
-  (ivy-mode 1))
-
-(use-package swiper
-  :bind ("C-s" . swiper))
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :config (setq which-key-idle-delay 0.3))
-
-(use-package ivy-rich
-  :init (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
+;; Load theme
+(let ((theme 'an-old-hope)
+      (url "https://raw.githubusercontent.com/mohkale/an-old-hope-emacs/master/an-old-hope-theme.el")
+      (file "~/.emacs.d/an-old-hope-theme.el"))
+  (unless (file-exists-p file)
+    (url-copy-file url file))
+  (load-theme theme))
 
 
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/Projects")
-    (setq projectile-project-search-path '("~/Projects")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-;;; Rust stuff
-(use-package flycheck
-  :hook (prog-mode . flycheck-mode))
-
-(use-package company
-  :hook (prog-mode . company-mode)
-  :config (setq company-tooltip-align-annotations t)
-          (setq company-minimum-prefix-length 1))
-
-(use-package lsp-mode
-  :commands lsp)
-;  :config (require 'lsp-clients))
-
-(use-package lsp-ui)
-
-(use-package toml-mode)
-
-(use-package rust-mode
-  :hook (rust-mode . lsp))
-
-;; Add keybindings for interacting with Cargo
-(use-package cargo
-  :hook (rust-mode . cargo-minor-mode))
-
-(use-package flycheck-rust
-  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-
-(use-package magit)
-;  :custom
-;  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-(use-package forge)
-
-(defun molion/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-;; Org Mode Configuration ------------------------------------------------------
-
-(defun molion/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.5)
-                  (org-level-2 . 1.2)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Courier New" :weight 'regular :height (cdr face)))
-
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-
-(use-package org
-  :hook (org-mode . molion/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-  (molion/org-font-setup))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . molion/org-mode-visual-fill))
-
-(use-package markdown-mode
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+;; Use Helm
+(use-package helm)
